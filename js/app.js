ee546178dc69111a7346b8d108eb84de8bcd6457
Gilles_Elan <gilles.muess@elan-formation.fr>
2020-07-23 15:21:55 +0200

    JS Jouable
    Manque les conditions de victoire

--- js/app.js
+++ js/app.js
@@ -1,17 +1,40 @@
-const joueurs = ['X', 'O']
-let joueurActif = joueurs[0],
-    cells = document.querySelectorAll('td')
-// console.log(cells)
-for(let cell of cells){
-    // console.log(cell)
-    cell.addEventListener('click', () => {
-        // console.log(this.innerHTML)
-        if(joueurActif === joueurs[0]){
-            this.innerHTML = joueurs[0]
-            joueurActif = joueurs[1]
-        } else {
-            this.innerHTML = joueurs[1]
-            joueurActif = joueurs[0]
+const symboles = {  //Identification du symbole des joueurs
+    1: 'X',
+    2: 'O'
+},
+    joueur1 = 1,
+    joueur2 = 2 
+let cells = document.querySelectorAll('td'), //Identification des cellules de jeu
+    joueurActif = joueur1, //Indication du premier joueur
+    count = 0
+
+//Ajout des actions au clic sur tous les TD
+for (let cell of cells) {
+    cell.addEventListener('click', function () {
+        play(this)
+        count++
+        if(count>=5){
+            endGame(this)
         }
     })
+}
+
+//Fonction permettant de poser une croix ou un rond (avec alternance auto)
+let play = elem => {
+    if(elem.className === ''){
+        elem.className = "light"
+        elem.innerHTML = symboles[joueurActif]
+        if (joueurActif == 1) {
+            joueurActif = joueur2
+        } else {
+            joueurActif = joueur1
+        }
+    } else {
+        alert('La case est déjà prise, joue ailleurs')
+    }
+}
+
+//Fonction déterminant la fin de partie
+let endGame = elem => {
+
 }
